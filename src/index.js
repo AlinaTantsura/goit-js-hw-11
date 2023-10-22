6933e3781eb8f7369bb8e8af37168d422a28e7d3
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-10-22 14:34:38 +0300

    Summary

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const API_KEY = '40200855-3a192acac81e17bb872cbbb4e';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const searchForm = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const input = searchForm.elements.searchQuery;
+let searchValue;
+
+input.addEventListener('input', handleInput);
+searchForm.addEventListener('submit', handleSubmit);
+
+
+function handleInput(event){
+    searchValue = event.currentTarget.value;
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    fetch(`${BASE_URL}?key=${API_KEY}&q=${searchValue}&image_type=horizontal&safesearch=true`)
+    .then(resp => 
+       {if(!resp.ok){
+        throw new Error()}
+    return resp.json()} ).then(arr => {
+        if(!arr.hits.length){
+            Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+            // console.log('Sorry, there are no images matching your search query. Please try again.');
+        }
+       const markup = arr.hits.map(item => `<div class="photo-card">
+        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            ${item.likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            ${item.views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            ${item.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            ${item.downloads}
+          </p>
+        </div>
+      </div>`).join('')
+      gallery.innerHTML = markup;
+    }).catch(() =>  Notify.failure('Oops! Something went wrong! Try reloading the page!'));
+}
