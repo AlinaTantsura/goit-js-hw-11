4b8bf1afa7d42474e8af16524a21238040eae09d
AlinaTantsura <alinatanst@gmail.com>
2023-10-23 17:30:21 +0300

    Axios-requests

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const API_KEY = '40200855-3a192acac81e17bb872cbbb4e';
-const BASE_URL = 'https://pixabay.com/api/';
-
 const searchForm = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const input = searchForm.elements.searchQuery;
 let searchValue;
+let page = 1;
+const loadMoreBtn = document.querySelector('.load-more');
 
+const API_KEY = '40200855-3a192acac81e17bb872cbbb4e';
+// const BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+// axios.defaults.headers.common['key'] = '40200855-3a192acac81e17bb872cbbb4e';
+// axios.defaults.headers.common['image_type'] = 'horizontal';
+// axios.defaults.headers.common['safesearch'] = 'true';
 input.addEventListener('input', handleInput);
 searchForm.addEventListener('submit', handleSubmit);
 
@@ -17,40 +22,89 @@ function handleInput(event){
     searchValue = event.currentTarget.value;
 }
 
-function handleSubmit(event){
-    event.preventDefault();
-    fetch(`${BASE_URL}?key=${API_KEY}&q=${searchValue}&image_type=horizontal&safesearch=true`)
-    .then(resp => 
-       {if(!resp.ok){
-        throw new Error()}
-    return resp.json()} ).then(arr => {
-      console.log(arr);
-        if(!arr.hits.length){
-            Notify.warning('Sorry, there are no images matching your search query. Please try again.');
-            return;// console.log('Sorry, there are no images matching your search query. Please try again.');
-        }
-        Notify.success(`Hooray! We found ${arr.totalHits} images.`);
-       const markup = arr.hits.map(item => `<div class="photo-card">
-        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
-        <div class="info">
-          <p class="info-item">
-            <b>Likes</b>
-            ${item.likes}
-          </p>
-          <p class="info-item">
-            <b>Views</b>
-            ${item.views}
-          </p>
-          <p class="info-item">
-            <b>Comments</b>
-            ${item.comments}
-          </p>
-          <p class="info-item">
-            <b>Downloads</b>
-            ${item.downloads}
-          </p>
-        </div>
-      </div>`).join('')
-      gallery.innerHTML = markup;
-    }).catch(() =>  Notify.failure('Oops! Something went wrong! Try reloading the page!'));
+async function handleSubmit(event) {
+  event.preventDefault();
+  try {
+    const resp = await fetchPictures();
+    if(!resp.data.hits.length){
+      Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+          return;// console.log('Sorry, there are no images matching your search query. Please try again.');
+      }
+      Notify.success(`Hooray! We found ${resp.data.totalHits} images.`);
+    console.log(resp.data.hits);
+    renderPictureCard(resp.data.hits);
+    loadMoreBtn.classList.remove('hidden');
+  }
+  catch{
+    Notify.failure('Oops! Something went wrong! Try reloading the page!');
+  }
 }
+
+async function fetchPictures(){
+    return await axios.get(`?key=${API_KEY}&q=${searchValue}&image_type=horizontal&safesearch=true&per_page=40&page=${page}`);
+ 
+  }
+function renderPictureCard(arr) {
+  const markup = arr.map(item => `<div class="photo-card">
+         <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+         <div class="info">
+           <p class="info-item">
+             <b>Likes</b>
+             ${item.likes}
+           </p>
+           <p class="info-item">
+             <b>Views</b>
+             ${item.views}
+           </p>
+           <p class="info-item">
+             <b>Comments</b>
+             ${item.comments}
+           </p>
+           <p class="info-item">
+             <b>Downloads</b>
+             ${item.downloads}
+           </p>
+       </div>
+       </div>`).join('')
+      gallery.innerHTML = markup;
+}
+// const API_KEY = '40200855-3a192acac81e17bb872cbbb4e';
+// const BASE_URL = 'https://pixabay.com/api/';
+
+// function handleSubmit(event){
+//     event.preventDefault();
+//     fetch(`${BASE_URL}?key=${API_KEY}&q=${searchValue}&image_type=horizontal&safesearch=true`)
+//     .then(resp => 
+//        {if(!resp.ok){
+//         throw new Error()}
+//     return resp.json()} ).then(arr => {
+//       console.log(arr);
+//         if(!arr.hits.length){
+//             Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+//             return;// console.log('Sorry, there are no images matching your search query. Please try again.');
+//         }
+//         Notify.success(`Hooray! We found ${arr.totalHits} images.`);
+//        const markup = arr.hits.map(item => `<div class="photo-card">
+//         <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+//         <div class="info">
+//           <p class="info-item">
+//             <b>Likes</b>
+//             ${item.likes}
+//           </p>
+//           <p class="info-item">
+//             <b>Views</b>
+//             ${item.views}
+//           </p>
+//           <p class="info-item">
+//             <b>Comments</b>
+//             ${item.comments}
+//           </p>
+//           <p class="info-item">
+//             <b>Downloads</b>
+//             ${item.downloads}
+//           </p>
+//         </div>
+//       </div>`).join('')
+//       gallery.innerHTML = markup;
+//     }).catch(() =>  Notify.failure('Oops! Something went wrong! Try reloading the page!'));
+// }
